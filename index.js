ed98b7292f5b50ea10f448aef8ca149503f9632e
James Keveren <james@keve.ren>
2020-10-22 19:50:45 +0200

    prevent popup blocker warning and cleanup

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const updateAnchor = anchor => {
-// 	if (anchor instanceof HTMLAnchorElement) {
-// 		const subredditMatch = anchor.href.match(/^https:\/\/www.reddit.com\/r\/(?<subreddit>[\w\d]{3,21})\/?$/)
-// 		if (subredditMatch && subredditMatch.groups.subreddit) {
-// 			console.log(anchor);
-// 			anchor.href = `https://www.reddit.com/r/${subredditMatch.groups.subreddit}/top/t=all`;
-// 		}
-// 	}
-// };
-
-// const mutationObserver = new MutationObserver(mutationRecord => {
-// 	console.log(mutationRecord);
-// 	if (muationRecord.type === 'attributes') {
-// 		updateAnchor(mutationRecord.target);
-// 	}
-// });
-// mutationObserver.observe(document.body, {
-// 	subtree: true;
-// });
-
-// const anchors = document.getElementsByTagName('a');
-// for (const anchor of anchors) {
-// 	updateAnchor(anchor);
-// };
-
 const findParentAnchor = target => {
 	if (target instanceof HTMLAnchorElement) {
 		return target;
@@ -43,12 +18,8 @@ document.addEventListener('auxclick', event => {
 		return;
 	}
 	const subredditMatch = anchor.href.match(/^https:\/\/www.reddit.com\/r\/(?<subreddit>[\w\d]{3,21})\/?$/);
-	console.log(subredditMatch);
 	if (subredditMatch && subredditMatch.groups.subreddit) {
-		console.log(subredditMatch.groups.subreddit);
-		// event.preventDefault();
-		// event.stopPropagation();
-		// event.stopImmediatePropagation();
+		event.preventDefault();
 		open(`https://www.reddit.com/r/${subredditMatch.groups.subreddit}/top/?t=all`, '_blank');
 	}
 });
